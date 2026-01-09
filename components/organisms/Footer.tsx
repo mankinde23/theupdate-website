1fbf0cbed435a6cd26aecac26618773bb4778185
mankinde23 <damilolamakinde493@gmail.com>
2026-01-09 02:21:17 +0100

    worked on the footer section

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -4,42 +4,60 @@ import logo from "@/public//logo.svg";
 import { Separator } from "../ui/separator";
 import { Instagram, Youtube } from "lucide-react";
 import { XLogo } from "@phosphor-icons/react/ssr";
+import update from "@/public/images/the-updatet.png";
 export default function Footer() {
   return (
     <>
       <div>
-        <div className="bg-[#191919]  mt-[2.5625rem] max-lg:mt-0 text-[#FFFFFF] font-sans">
-          <div className="max-w-7xl mx-auto px-6 py-16">
-            <div className="flex justify-center ">
-              <Image
-                src={logo}
-                alt="hero-image"
-                className="w-[17.8125rem] h-[6.6875rem] max-lg:h-[3.0625rem] max-lg:w-[8.8175rem]"
-              />
-            </div>
-            <Separator className="bg-[#9E9E9E] border-[0.0625rem] bg-transparent mt-[2.375rem] text-[#9E9E9E99]" />
-            <div className="flex flex-col mt-[3.625rem] gap-[4.4375rem]">
+        <div className="bg-[#222222]  mt-[105px] mb-[45px] max-lg:mt-0 text-[#FFFFFF] font-sans mx-[45px] rounded-[44px]  max-lg:py-9 p-[61px]">
+          <div className="flex flex-row gap-[148px] max-lg:flex-col max-lg:gap-[55px]  ">
+            <div className="flex flex-col gap-[48px]">
               <div>
-                <ul className="flex flex-row justify-between max-lg:text-[13px]">
+                <div className="w-[369px]">
+                  <Image src={update} alt="nation wide logo" className="" />
+                </div>
+                <div className="text-[#FFFFFFB2] text-[14px] font-normal mt-[16px]">
+                  Book Fast. Book smart. Book your way.
+                </div>
+              </div>
+              <div className="flex flex-col gap-6.5">
+                <div>Follow Us:</div>
+                <div className="flex flex-row gap-7.25 ">
+                  <Youtube size={22} fill="" color="#FFFFFF" />
+                  <XLogo size={22} />
+                  <Instagram size={22} />
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-row justify-between w-full">
+              <div className="flex gap-4 flex-col">
+                <div className="text-[#FFFFFF] text-[18px] font-medium">
+                  Quick Links
+                </div>
+                <ul className="flex flex-col gap-3 text-[#FFFFFFB2] text-[16px] font-normal">
                   <li>About</li>
                   <li>Services</li>
                   <li>Contact</li>
                   <li>Newsletter</li>
                 </ul>
               </div>
-              <div className="flex flex-row gap-[96px] justify-center">
-                <Youtube size={22} />
-                <XLogo size={22} />
-                <Instagram size={22} />
+              <div className="flex gap-4 flex-col">
+                <div className="text-[#FFFFFF] text-[18px] font-medium">
+                  More Info
+                </div>
+                <ul className="flex flex-col gap-3 text-[#FFFFFFB2] text-[16px] font-normal">
+                  <li>News</li>
+                  <li>New features</li>
+                  <li>Announcements</li>
+                  <li>The Release</li>
+                </ul>
               </div>
             </div>
           </div>
 
-          {/* Bottom Copyright */}
-          <div className="border-t  border-t-[#0000001F] b-t-border-[1px] py-6 border-transparent">
-            <p className="text-center text-[11px] text-[#FFFFFF] font-normal">
-              Copyright © 2025 Nationwide Realty Real Estate, All rights
-              reserved.
+          <div className="border-t  border-t-[#FFFFFF1C] b-t-border-[1px] pt-[47px] border-transparent mt-11.75">
+            <p className="text-center text-[12px] text-[#FFFFFFB2] font-normal">
+              © 2026 The Update App. All rights reserved.
             </p>
           </div>
         </div>
